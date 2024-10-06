ee52633304e54cbc07505e14a694277472b8fe67
Stefan2314 <quequeg.liviu@gmail.com>
2024-10-07 01:06:03 +0300

    Add files via upload

---
+++ medusa-config.js
@@ -0,0 +1,18 @@
+const { loadEnv, defineConfig } = require('@medusajs/utils')
+
+loadEnv(process.env.NODE_ENV, process.cwd())
+
+console.log(process.env.MEDUSA_BACKEND_HOST)
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      host:process.env.MEDUSA_BACKEND_HOST,
+    }
+  }
+})
